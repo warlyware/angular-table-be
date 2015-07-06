b06e313d7405c862f8ac2d0d0e1710638674ba58
warlyware <danward@gmail.com>
2015-07-06 10:07:11 -0700

    Add endpoint for data.json

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../data.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get("/data", function(req, res) {
+  console.log(data);
+  res.json(data);
 });
 
 module.exports = router;
